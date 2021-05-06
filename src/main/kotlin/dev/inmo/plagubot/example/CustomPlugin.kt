30eff9ace9118d04846c1a2d21a0c340cab00fae
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-05-06 12:29:37 +0600

    update dependencies, add flush opportunity in custom plugin and more obvious handling of exceptions

--- src/main/kotlin/dev/inmo/plagubot/example/CustomPlugin.kt
+++ src/main/kotlin/dev/inmo/plagubot/example/CustomPlugin.kt
@@ -1,21 +1,30 @@
 package dev.inmo.plagubot.example
 
-import dev.inmo.micro_utils.coroutines.defaultSafelyWithoutExceptionHandler
-import dev.inmo.micro_utils.coroutines.subscribeSafelyWithoutExceptions
+import dev.inmo.micro_utils.coroutines.*
 import dev.inmo.plagubot.Plugin
 import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
+import dev.inmo.tgbotapi.extensions.utils.updates.retrieving.retrieveAccumulatedUpdates
 import kotlinx.coroutines.flow.onEach
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.Database
 
 @Serializable
-class CustomPlugin : Plugin {
+class CustomPlugin(
+    private val flushUpdates: Boolean = false
+) : Plugin {
     override suspend fun BehaviourContext.invoke(database: Database, params: Map<String, Any>) {
+        if (flushUpdates) {
+            retrieveAccumulatedUpdates {
+                // just flush
+            }
+        }
         allUpdatesFlow.subscribeSafelyWithoutExceptions(this) {
             println(it)
         }
-        defaultSafelyWithoutExceptionHandler = {
+        val currentDefaultSafelyExceptionHandler = defaultSafelyExceptionHandler
+        defaultSafelyExceptionHandler = {
             it.printStackTrace()
+            currentDefaultSafelyExceptionHandler(it)
         }
     }
 }
